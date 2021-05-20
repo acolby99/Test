113a815fcd35ba28ace4f299bf801a5354efcd30
Colby <colalef@dunwoody.edu>
2021-05-20 07:27:02 -0500

    A message saying what changes were made or whatever

---
+++ Untitled-1.py
@@ -0,0 +1,50 @@
+import os
+import pathlib
+
+os.system('cls')
+
+def GetState():
+    state = input("Please enter the name of a state: ")
+    return state;
+
+def FormalState(state):
+    if len(state) < 8:
+        just = state.ljust(8, "*")
+        return just
+    else:
+        return state;
+
+print ("1. Get information and display to screen")
+print ("2. Call user created functions")
+print ("3. Write list of files to file")
+print ("4. Read specified file")
+
+answer = input ("Please enter the number of the menu item you want to perform: ")
+
+if answer() == "1":
+    companyName = "Dunwoody College"
+    programName = "Computer Networking"
+    uName = os.environ['UserName']
+    classFirst = input ("Enter your first class: ")
+    classSecond = input ("Enter your second class: ")
+    print ("Logged on as", uName, "at", companyName, "in department:", programName)
+    print ("My first class is", classFirst, "and my second class is", classSecond)
+
+elif answer() == "2":
+    state = GetState()
+    newState = FormalState(state)
+    print ("State was", state, "and is now", state)
+
+elif answer() == "3":
+    fileName = input ("Enter a file name to create: ")
+    fList = []
+    dList = []
+    for p in pathlib.Path('.').iterdir():
+        if p.is_file():
+            fList.append(p)
+        else:
+            dList.append(p)
+
+elif answer() == "4":
+    fRead = input ("Enter a filename to read")
+    fileContents = fRead.read()
